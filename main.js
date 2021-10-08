8c733228b34ec06c81e9b5574d0973ad174e116c
serdar87 <83467033+serdar87@users.noreply.github.com>
2021-10-08 15:48:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".fas").addEventListener("click", function () {
+  document.querySelector(".nav").classList.toggle("navmobile");
+  document.querySelector(".arrowimage").classList.toggle("hidden");
+});
