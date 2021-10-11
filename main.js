7031b232c1fedfeb56d4cd9988e8977c3a09784c
serdar87 <83467033+serdar87@users.noreply.github.com>
2021-10-11 15:26:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ document.querySelector(".fas").addEventListener("click", function () {
   document.querySelector(".nav").classList.toggle("navmobile");
   document.querySelector(".arrowimage").classList.toggle("hidden");
 });
+
+document.addEventListener("click", function (e) {
+  if (
+    !e.target.matches(".nav") &&
+    !e.target.matches(".fas") &&
+    document.querySelector(".nav").classList.contains("navmobile")
+  )
+    document.querySelector(".nav").classList.remove("navmobile");
+});
