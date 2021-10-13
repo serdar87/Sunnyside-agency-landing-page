b93b44a96be7d3f0b50ebad38dd4183c54206f85
serdar87 <83467033+serdar87@users.noreply.github.com>
2021-10-13 15:00:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+let mobileNav = document.querySelector(".nav");
 document.querySelector(".fas").addEventListener("click", function () {
-  document.querySelector(".nav").classList.toggle("navmobile");
+  if (mobileNav.style.display === "block") {
+    mobileNav.style.display = "none";
+  } else {
+    mobileNav.style.display = "block";
+  }
   document.querySelector(".arrowimage").classList.toggle("hidden");
 });
 
-document.addEventListener("click", function (e) {
-  if (
-    !e.target.matches(".nav") &&
-    !e.target.matches(".fas") &&
-    document.querySelector(".nav").classList.contains("navmobile")
-  )
-    document.querySelector(".nav").classList.remove("navmobile");
+document.body.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (!e.target.matches("ul") && !e.target.matches(".fas")) {
+    document.querySelector(".arrowimage").classList.remove("hidden");
+    mobileNav.style.display = "none";
+  }
 });
